99074d5059e8fadf34da2cc193394a4f2f57585b
Mthunzi Tom <mthunzitom.10@gmail.com>
2023-07-01 22:02:24 +0200

    documented code, refactored code to make it simple to understand

--- app/shoe-catalog.app.js
+++ app/shoe-catalog.app.js
@@ -253,15 +253,21 @@ function ShoeCatalogApp(updatedShoesData) {
 
     // functions for the searching of a shoe in the user interface
 
+    // create an empty variable to store the shoes
     let shoes = [];
 
     function searchForShoes(shoeBrand, shoeColor, shoeSize, priceRange) {
+        // iterate over the length of the shoesData object then..
         for (let i = 0; i < shoesData.length; i++) {
+            // grab the current shoe in the object and...
             const shoe = shoesData[i];
 
             // case : searching for a shoe brand
             // case : searching for a shoe calor
             // case : searching for a shoe size
+
+            // compare the shoe brand, shoe color and shoe size if equals to the shoes data in the 
+            // shoesData object, if true then..
 
             if (
                 (shoeBrand === shoe.brand && shoeColor === null && shoeSize === null) ||
@@ -269,9 +275,12 @@ function ShoeCatalogApp(updatedShoesData) {
                 (shoeBrand === shoe.brand && shoeColor === shoe.color && shoeSize === null)
             ) {
 
-                // I pushed the all the shoe data because I want to show the data into the screen
+                // push the shoe into the shoes array
                 shoes.push(shoe);
             };
+
+            // compare the shoe brand, shoe color and shoe size if equals to the shoes data in the 
+            // shoesData object, if true then..
 
             if (
                 (shoeBrand === null && shoeColor === null && shoe.size.includes(shoeSize)) ||
@@ -279,15 +288,14 @@ function ShoeCatalogApp(updatedShoesData) {
                 (shoeBrand === shoe.brand && shoeColor === shoe.color && shoe.size.includes(shoeSize))
             ) {
 
-                // I pushed the all the shoe data because I want to show the data into the screen
+                // push the shoe into the shoes array
                 shoes.push(shoe);
             };
 
             // create logic for when the user decides to search for shoes according to their price range below:
-            
+
         };
     };
-
     // function to show the shoes that the user searched for
 
     function foundShoes() {
@@ -296,183 +304,141 @@ function ShoeCatalogApp(updatedShoesData) {
             I will loop over the length of the that contains shoes data, shoesData object and push every shoe object that I find
         */
 
+        // check if the shoes array length is zero and...
         if (shoes.length === 0) {
+            // create an empty array
             let allOfTheShoes = [];
 
+            // loop over the length of the shoes data then...
             for (let i = 0; i < shoesData.length; i++) {
                 const shoe = shoesData[i];
 
+                // push all the shoes into the empty array
                 allOfTheShoes.push(shoe);
             };
-
+            // return the array of shoes
             return allOfTheShoes;
         };
-
-
         // return the shoes that the user searched for
-
         return shoes;
     };
 
     // functions for the shopping cart in the user interface
 
     // variables to store the cost of shoes and the number of shoes added to cart
-    let addedNumShoesToCart = 0;
     let shoesTotalPrice = 0;
-
-    /* 
-        addToCart() and removeFromCart() methods keep track of the number of shoes added and removed from the cart
-    */
-
-    function addToCart() {
-        // increment the shopping cart by one
-        addedNumShoesToCart++;
-    }
-
-    function removeFromCart() {
-        // decrement the shoes added in the shopping cart by one
-        if (addedNumShoesToCart > 0) addedNumShoesToCart--;
-    }
 
     // variable to store the shoes added to cart
     let shoesInCart = [];
 
-    function addToCartFunctionality() {
-        /*
-            when the user clicks the <add-to-cart> span element which will be shown everytime a user
-            use a mouse to hover over the image card element in the user interface, the function inStockDecrement() 
-            will decrease the "in-stock" value by one and the function addShoeCost() will add the cost of a shoe to the shoesTotalCost
-        */
+    /*
+        when the user clicks the <add-to-cart> span element which will be shown everytime a user
+        use a mouse to hover over the image card element in the user interface, the function inStockDecrement() 
+        will decrease the "in-stock" value by one and the function addShoeCost() will add the cost of a shoe to the shoesTotalCost
+    */
 
-        function decreaseInStock(shoeIdentifier) {
-            for (let i = 0; i < shoesData.length; i++) {
-                const shoe = shoesData[i];
+    function addShoeToCart(shoeIdentifier) {
+        // iterate over the length of the shoes data then...
+        for (let i = 0; i < shoesData.length; i++) {
+            // grab a shoe
+            const shoe = shoesData[i];
 
-                if (shoe.id === shoeIdentifier) {
+            // check if the a shoe id is equal to the shoe id we'll get when the user clicks the add to cart element in the 
+            // user interface then...
+
+            if (shoe.id === shoeIdentifier) {
+
+                // only add a shoe to the shopping cart when the in-stock value is greater than zero
+                if (shoe["in-stock"] > 0) {
                     // decrement the "in-stock" value in the shoesData object
                     shoe["in-stock"]--;
-                };
-            };
-        };
 
-        function addShoeCost(shoeIdentifier) {
-            for (let j = 0; j < shoesData.length; j++) {
-                const shoe = shoesData[j];
-
-                if (shoe.id === shoeIdentifier) {
                     // add the cost of a shoe to the shoesTotalCost variable by the shoe price
                     shoesTotalPrice += shoe.price;
-                };
+
+                    // add a shoe to the shopping cart
+                    shoesInCart.push(shoe);
+                }
             };
         };
-
-        function addShoeToCart(shoeIdentifier) {
-            for (let k = 0; k < shoesData.length; k++) {
-                const shoe = shoesData[k];
-
-                if (shoe.id === shoeIdentifier) {
-                    /* 
-                        check if the shoe contained in the shoe variable is in the shoesInCart array,
-                        if it is contained do not append it into the array just skip into the next element 
-                        else if it is not there append it
-                    */
-
-                    if (shoesInCart.includes(shoe)) {
-                        continue;
-                    } else {
-                        shoesInCart.push(shoe);
-                    };
-                };
-
-            };
-        };
-
-        return {
-            decreaseInStock,
-            addShoeCost,
-            addShoeToCart,
-        };
-
     };
 
-    function removeFromCartFunctionality() {
-        /*
-            when the user clicks the an icon to remove a shoe item in the shopping cart,
-            the function inStockIncrement will increase the "in-stock" value in the shoesData object and the function minusShoesCost()
-            will decrease the shoesTotalPrice variable by the shoe price removed from the cart
-        */
+    /*
+        when the user clicks an icon to remove a shoe item in the shopping cart,
+        the function removeShart will increase the "in-stock" value in the shoesData object and
+        will decrease the shoesTotalPrice variable and also remove a shoe from the cart
+    */
 
-        function increaseInStock(shoeIdentifier) {
-            for (let i = 0; i < shoesData.length; i++) {
-                const shoe = shoesData[i];
+    function removeShoeFromCart(shoeIdentifier) {
+        // iterate over the length of the shoes data then...
+        for (let i = 0; i < shoesData.length; i++) {
+            // grab a shoe
+            const shoe = shoesData[i];
 
-                if (shoe.id === shoeIdentifier) {
-                    // increment the "in-stock" value in the shoesData object
-                    shoe["in-stock"]++;
-                };
+            // check if the a shoe id is equal to the shoe id we'll get when the user clicks the remove from cart element in the 
+            // user interface then...
+
+            if (shoe.id === shoeIdentifier) {
+
+                // iterate over the length of the shoes added in the cart and then...
+                for (let j = 0; j < shoesInCart.length; j++) {
+                    // grab a shoe added in the shoping cart
+                    const shoeAddedInCart = shoesInCart[j];
+
+                    // check if the shoe id in the shopping cart is equal to the disired shoe id and...
+                    if (shoeAddedInCart.id === shoeIdentifier) {
+
+                        // when the shoesInCart array includes the current shoe in the shoesData object then...
+                        if (shoesInCart.includes(shoe)) {
+                            // increment the "in-stock" value in the shoesData object,
+                            shoe["in-stock"]++;
+                            // minus the cost of a shoe from the shoesTotalCost variable by the shoe price and
+                            shoesTotalPrice -= shoe.price;
+
+                            // remove a shoe in the shoesInCart array
+                            shoesInCart.splice(j, 1);
+                        }
+                    }
+                }
             };
-        };
-
-        function minusShoeCost(shoeIdentifier) {
-            for (let j = 0; j < shoesData.length; j++) {
-                const shoe = shoesData[j];
-
-                if (shoe.id === shoeIdentifier) {
-                    // minus the cost of a shoe from the shoesTotalCost variable by the shoe price
-                    shoesTotalPrice -= shoe.price;
-                };
-            };
-        };
-
-        function removeShoeFromCart(shoeIdentifier) {
-
-            /* 
-                code logic will help to remove the shoe in the user interface when the user decides to remove a shoe from
-                the shopping cart
-            */
-
-            // iterate over the length of the shoesInCart array
-            for (let k = 0; k < shoesInCart.length; k++) {
-                // grab the current shoe object
-                const shoe = shoesInCart[k];
-
-                // check if the current shoe object id equals the one I want to decrement
-                if (shoe.id === shoeIdentifier) {
-
-                    // remove the element in the shoesInCart array that contains the shoe
-                    shoesInCart.splice(k, 1);
-                };
-            };
-        };
-
-        return {
-            increaseInStock,
-            minusShoeCost,
-            removeShoeFromCart,
         };
     };
 
     // shopping cart get functions
 
-    function getNumAddedToCart() {
-        return addedNumShoesToCart;
+    // function keep track of the number of shoes added to the shopping cart
+
+    function addedToCart() {
+        // add the number in the shopping cart by using the length of the array
+        return shoesInCart.length;
     };
+
+    // function to keep track of the shoes price in the shopping cart
 
     function getTotalShoesPrice() {
         return shoesTotalPrice;
     };
 
     /*
-        avaliablbeShoeStock() function helps to be able to get the "in-stock" value by giving the function a 
+        avaliableShoeStock() function helps to be able to get the "in-stock" value by giving the function a 
         shoeIdentifier as an argument, so that it can can compare the shoesData object id's with the shoeIdentifier
         and get the shoe "in-stock" value when they are equal by returning it
+
+        The availableShoeStock() function I will be using it to test if I am getting the correct in-stock value
+        in the shoesData object, after adding and removing a shoe in the shoesInCart array
     */
 
     function avaliableShoeStock(shoeIdentifier) {
+
+        // iterate over the length of the shoesData object then...
         for (let i = 0; i < shoesData.length; i++) {
+            // grab the current shoe in the shoesData object and...
             const shoe = shoesData[i];
 
+            // compare the shoe id if it equals to the disired shoe id and..
             if (shoe.id === shoeIdentifier) {
+
+                // return the in-stock value
                 return shoe["in-stock"];
             };
         };
@@ -484,32 +450,27 @@ function ShoeCatalogApp(updatedShoesData) {
         return shoesInCart;
     };
 
-
-
     // get function for setting the shoes data in the local storage
 
     function getShoesData() {
         return shoesData;
     };
 
-
     // functions return 
 
     return {
-
-        // shopping cart return functions
-        addToCart,
-        removeFromCart,
-        addToCartFunctionality,
-        removeFromCartFunctionality,
-        getNumAddedToCart,
-        getTotalShoesPrice,
-        avaliableShoeStock,
-        shoesAddedToCart,
-
         // searching of a shoe functions
         searchForShoes,
         foundShoes,
+
+        // shopping cart return functions
+        // decreaseCart,
+        addShoeToCart,
+        removeShoeFromCart,
+        addedToCart,
+        getTotalShoesPrice,
+        avaliableShoeStock,
+        shoesAddedToCart,
 
         // local storage function
         getShoesData,
